6649a9377b6cf89bdba13dfafa52a6e60ba402b8
Starknet Dev <starknetdev@gmail.com>
2024-09-02 16:30:39 -0400

    add a countdown based on a target time

---
+++ src/containers/Countdown.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+
+const Countdown = () => {
+  const [loadingMessage, setLoadingMessage] = useState("");
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const messages = [
+    "Sharpening pixelated swords...",
+    "Brewing potions of impatience...",
+    "Waking up sleepy dragons...",
+    "Polishing lucky dice...",
+    "Dusting off ancient spell books...",
+    "Tuning magical instruments...",
+    "Feeding hungry treasure chests...",
+    "Inflating health potions...",
+    "Rehearsing NPC dialogue...",
+    "Calibrating difficulty sliders...",
+    "Hiding secret passages...",
+    "Planting legendary loot...",
+    "Summoning RNG spirits...",
+    "Buffing hero armor to a shine...",
+    "Preparing epic plot twists...",
+    "Generating witty one-liners...",
+    "Unleashing mischievous minions...",
+    "Crafting cryptic riddles...",
+    "Warming up boss battle music...",
+    "Sharpening critical hit calculations...",
+  ];
+
+  // Set your target date here
+  const targetDate = new Date("2024-09-04T12:00:00").getTime();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const difference = targetDate - now;
+
+      if (difference > 0) {
+        setTimeLeft({
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor(
+            (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          ),
+          minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((difference % (1000 * 60)) / 1000),
+        });
+      } else {
+        clearInterval(interval);
+        // Handle countdown finished
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    setLoadingMessage(messages[randomIndex]);
+  }, []);
+
+  return (
+    <div className="absolute flex flex-col items-center justify-between inset-0 z-50 w-full h-full gap-20 p-20 bg-black/50 bg-[url('/scenes/opening.png')] bg-cover bg-center bg-no-repeat bg-blend-overlay">
+      <h1 className="text-6xl uppercase">Claiming Opens</h1>
+      <div className="text-8xl mb-8 text-terminal-yellow text-center uppercase">
+        {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m{" "}
+        {timeLeft.seconds}s
+      </div>
+      <h2 className="text-4xl uppercase">{loadingMessage}</h2>
+    </div>
+  );
+};
+
+export default Countdown;
